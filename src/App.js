e5dcb56ceb47e7ea1be0e7a6a80ae9acd5deec11
aburk3 <aburk3@gmail.com>
2019-05-10 19:35:48 -0500

    State added to App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from "react";
 import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Austin", age: 24 },
+      { name: "Mallory", age: 22 },
+      { name: "Dylan", age: 25 }
+    ]
+  };
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <button>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+          My Hobbies: Racing
+        </Person>
       </div>
     );
   }
