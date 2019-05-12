9f9c17732316cd91c9a4018032c6b29b0ebb36cc
aburk3 <aburk3@gmail.com>
2019-05-12 14:42:25 -0500

    Added dynamic styling to the Toggle Persons button

--- src/App.js
+++ src/App.js
@@ -57,7 +57,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: "white",
+      backgroundColor: "green",
+      color: "white",
       font: "inherit",
       border: "1x solid blue",
       padding: "8px",
@@ -83,6 +84,8 @@ class App extends Component {
           })}
         </div>
       );
+
+      style.backgroundColor = "red";
     }
 
     return (
