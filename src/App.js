8a8a2726b146306b32e3d2a3873b1c60fc6d7334
aburk3 <aburk3@gmail.com>
2019-05-13 05:16:59 -0500

    Added Error Boundary Component to see how errors can be thrown in Production builds

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import classes from "./App.module.css";
 import Person from "./Person/Person";
+import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
 
 class App extends Component {
   state = {
@@ -65,13 +66,14 @@ class App extends Component {
           {/* using javaScript to return valid JSX  */}
           {this.state.persons.map((person, index) => {
             return (
-              <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={event => this.nameChangedHandler(event, person.id)}
-              />
+              <ErrorBoundary key={person.id}>
+                <Person
+                  click={() => this.deletePersonHandler(index)}
+                  name={person.name}
+                  age={person.age}
+                  changed={event => this.nameChangedHandler(event, person.id)}
+                />
+              </ErrorBoundary>
             );
           })}
         </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,6 +2,11 @@ import React from "react";
 import classes from "./Person.module.css";
 
 const Person = props => {
+  const rnd = Math.random();
+
+  if (rnd > 0.7) {
+    throw new Error("Something went wrong");
+  }
   return (
     <div className={classes.Person}>
       <p onClick={props.click}>
