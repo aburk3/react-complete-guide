aefe2cc9273e1c9611e8ff4e28a55bd9369e941e
aburk3 <aburk3@gmail.com>
2019-05-13 05:28:07 -0500

    rearranged File structure to separate components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import classes from "./App.module.css";
-import Person from "./Person/Person";
-import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
+import Person from "../components/Persons/Person/Person";
 
 class App extends Component {
   state = {
@@ -66,14 +65,13 @@ class App extends Component {
           {/* using javaScript to return valid JSX  */}
           {this.state.persons.map((person, index) => {
             return (
-              <ErrorBoundary key={person.id}>
-                <Person
-                  click={() => this.deletePersonHandler(index)}
-                  name={person.name}
-                  age={person.age}
-                  changed={event => this.nameChangedHandler(event, person.id)}
-                />
-              </ErrorBoundary>
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={event => this.nameChangedHandler(event, person.id)}
+              />
             );
           })}
         </div>
@@ -90,11 +88,6 @@ class App extends Component {
     }
 
     return (
-      /**
-       * StyleRoot is needed by Radium for
-       * the media query used in the Person component
-       */
-
       <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
         <p className={assignedClasses.join(" ")}>This is really working!</p>
