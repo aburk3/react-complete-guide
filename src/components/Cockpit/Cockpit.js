b88483c73ece6f53016240403ce0b27888e879de
aburk3 <aburk3@gmail.com>
2019-05-15 05:54:06 -0500

    useEffect hook examples

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -2,9 +2,25 @@ import React, { useEffect } from "react";
 import classes from "./Cockpit.module.css";
 
 const Cockpit = props => {
+  // useEffect is componentDidMount and componentDidUpdate in one effect
   useEffect(() => {
     console.log("[Cockpit.js] useEffect");
     // Http request...
+    setTimeout(() => {
+      alert("Saved data to cloud!");
+    }, 1000);
+    return () => {
+      // runs the last time, since [] means only run once, this will run the first time
+      console.log("[Cockpit.js] cleanup work in useEffect");
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("[Cockpit.js] 2nd useEffect");
+    return () => {
+      console.log("[Cockpit.js] cleanup work in 2nd useEffect");
+    };
+    // No second argument (as above) so it will run for each update
   });
 
   const assignedClasses = [];
--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,8 @@ class App extends Component {
       { id: "asdf11", name: "Dylan", age: 26 }
     ],
     otherState: "some other value",
-    showPersons: false
+    showPersons: false,
+    showCockpit: true
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -96,12 +97,21 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
-        <Cockpit
-          title={this.props.appTitle}
-          showPersons={this.state.showPersons}
-          persons={this.state.persons}
-          clicked={this.togglePersonsHandler}
-        />
+        <button
+          onClick={() => {
+            this.setState({ showCockpit: false });
+          }}
+        >
+          Remove Cockpit
+        </button>
+        {this.state.showCockpit ? (
+          <Cockpit
+            title={this.props.appTitle}
+            showPersons={this.state.showPersons}
+            persons={this.state.persons}
+            clicked={this.togglePersonsHandler}
+          />
+        ) : null}
         {persons}
       </div>
     );
