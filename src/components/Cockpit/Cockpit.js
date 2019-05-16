c775addf007dbfa2db652f0fc8c5620fda873e8d
aburk3 <aburk3@gmail.com>
2019-05-16 06:34:36 -0500

    Refactored context and implemented contextType and useContext

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,9 +1,12 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useContext } from "react";
 import classes from "./Cockpit.module.css";
 import AuthContext from "../../context/auth-context";
 
 const Cockpit = props => {
   const toggleBtnRef = useRef(null);
+  const authContext = useContext(AuthContext);
+
+  console.log(authContext.authenticated);
 
   // useEffect is componentDidMount and componentDidUpdate in one effect
   useEffect(() => {
@@ -52,9 +55,8 @@ const Cockpit = props => {
       <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
         Toggle Persons
       </button>
-      <AuthContext.Consumer>
-        {context => <button onClick={context.login}>Log in</button>}
-      </AuthContext.Consumer>
+
+      <button onClick={authContext.login}>Log in</button>
     </div>
   );
 };
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -12,9 +12,14 @@ class Person extends Component {
     this.inputElementRef = React.createRef();
   }
 
+  // can only be used in Class based components
+  // functional components use `useContext`
+  static contextType = AuthContext;
+
   componentDidMount() {
     // this.inputElement.focus();
     this.inputElementRef.current.focus();
+    console.log(this.context.authenticated);
   }
 
   render() {
@@ -22,11 +27,12 @@ class Person extends Component {
 
     return (
       <Aux>
-        <AuthContext.Consumer>
-          {context =>
-            context.authenticated ? <p>Authenticated</p> : <p>Please log in</p>
-          }
-        </AuthContext.Consumer>
+        {this.context.authenticated ? (
+          <p>Authenticated</p>
+        ) : (
+          <p>Please log in</p>
+        )}
+
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
