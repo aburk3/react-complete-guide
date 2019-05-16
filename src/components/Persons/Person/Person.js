fcf7283a824d651205489eb1b86c39bf246e7714
aburk3 <aburk3@gmail.com>
2019-05-16 05:18:49 -0500

    change setState to ensure that it updates the current state

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import classes from "./Person.module.css";
 import Aux from "../../../hoc/Aux";
+import withClass from "../../../hoc/withClass";
+import classes from "./Person.module.css";
 
 class Person extends Component {
   render() {
@@ -22,4 +23,4 @@ class Person extends Component {
   }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
