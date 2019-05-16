623f015011f6a8a511d2b9265baac7c14fb7b1fe
aburk3 <aburk3@gmail.com>
2019-05-16 05:36:04 -0500

    Added ref to focus on the last Persons input field

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,6 +6,10 @@ import withClass from "../../../hoc/withClass";
 import classes from "./Person.module.css";
 
 class Person extends Component {
+  componentDidMount() {
+    this.inputElement.focus();
+  }
+
   render() {
     console.log("[Person.js] rendering...");
 
@@ -14,8 +18,12 @@ class Person extends Component {
         <p onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
-        <p>{this.props.children}</p>
+        <p key="i2">{this.props.children}</p>
         <input
+          key="i3"
+          ref={inputEl => {
+            this.inputElement = inputEl;
+          }}
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
